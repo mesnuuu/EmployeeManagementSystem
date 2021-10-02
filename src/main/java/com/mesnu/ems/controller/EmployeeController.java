40e5a84e8c7c0b36e647ff2e8ea0c84204d014b5
mesnuuu <mesnuuu@gmail.com>
2021-10-02 13:29:48 +0500

    02.3 - Add Employee into Db - Step 3 : "Process form data to save employee"
    	"Post/Redirect/Get" Pattern
    
    https://en.wikipedia.org/wiki/Post/Redirect/Get
    
    # need to fix empty entries. # validation

--- src/main/java/com/mesnu/ems/controller/EmployeeController.java
+++ src/main/java/com/mesnu/ems/controller/EmployeeController.java
@@ -1,13 +1,19 @@
 package com.mesnu.ems.controller;
+
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.mesnu.ems.entity.Employee;
 import com.mesnu.ems.service.EmployeeService;
-
 
 @Controller
 
@@ -23,27 +29,37 @@ public class EmployeeController {
 	// add mapping for "/list"
 
 	@GetMapping("/list")
-	public String listEmployees(Model theModel) { 
+	public String listEmployees(Model theModel) {
 
-		// get employees from db 
-	
-		List <Employee> theEmployees = employeeService.findAll();
+		// get employees from db
 
-		// add to the spring model 
+		List<Employee> theEmployees = employeeService.findAll();
+
+		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
 
 		return "/employees/list-employees";
 	}
-	
+
 	@GetMapping("/showFormForAdd")
 	public String showFormForAdd(Model theModel) {
-		
+
 		// create model attribute to bind form data
 		Employee theEmployee = new Employee();
-		
+
 		theModel.addAttribute("employee", theEmployee);
-		
+
 		return "/employees/employee-form";
 	}
-	
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+
+		// save the employee
+		employeeService.save(theEmployee);
+
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/employees/list";
+	}
+
 }
--- src/main/java/com/mesnu/ems/dao/EmployeeRepository.java
+++ src/main/java/com/mesnu/ems/dao/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.mesnu.ems.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mesnu.ems.entity.Employee;
@@ -7,5 +9,6 @@ import com.mesnu.ems.entity.Employee;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it ... no need to write any code LOL!
+	public List<Employee> findAllByOrderByLastNameAsc();
 	
 }
