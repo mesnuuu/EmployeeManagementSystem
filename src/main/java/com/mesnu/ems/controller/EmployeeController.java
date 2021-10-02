b98a292ac68f898d684343963c75ade316a6e8b7
mesnuuu <mesnuuu@gmail.com>
2021-10-02 11:40:59 +0500

    03 - Data from Mysql to Thymleaf view
    http://localhost:8080/employees/list

--- src/main/java/com/mesnu/ems/controller/EmployeeController.java
+++ src/main/java/com/mesnu/ems/controller/EmployeeController.java
@@ -1,3 +1,37 @@
 package com.mesnu.ems.controller;
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.mesnu.ems.entity.Employee;
+import com.mesnu.ems.service.EmployeeService;
+
+
+@Controller
+
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	private EmployeeService employeeService;
+
+	public EmployeeController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
+	}
+
+	// add mapping for "/list"
+
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+
+		// get employees from db 
+	
+		List <Employee> theEmployees = employeeService.findAll();
+
+		// add to the spring model 
+		theModel.addAttribute("employees", theEmployees);
+
+		return "list-employees";
+	}
+}
