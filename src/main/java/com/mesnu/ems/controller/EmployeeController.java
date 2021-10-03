749ee372c85bf3339eb3d2277b1c0bc23e0d8740
mesnuuu <mesnuuu@gmail.com>
2021-10-03 14:36:08 +0500

    04 - Thymleaf - DELETE EMPLOYEE

--- src/main/java/com/mesnu/ems/controller/EmployeeController.java
+++ src/main/java/com/mesnu/ems/controller/EmployeeController.java
@@ -67,6 +67,18 @@ public class EmployeeController {
 		return "employees/employee-form";			
 	}
 	
+	@GetMapping("/delete")
+	public String delete(@RequestParam("employeeId") int theId) {
+		
+		// delete the employee
+		employeeService.deleteById(theId);
+		
+		// redirect to /employees/list
+		return "redirect:/employees/list";
+		
+	}
+
+	
 
 	@PostMapping("/save")
 	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
