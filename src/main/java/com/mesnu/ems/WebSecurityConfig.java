8638e18009e8162ba165f6375bb6b942946adafa
mesnuuu <mesnuuu@gmail.com>
2021-10-05 23:58:18 +0500

    06.3 - Spring Security - Custom Login Form
    in memory auth.

---
+++ src/main/java/com/mesnu/ems/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.mesnu.ems;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/customLogin")
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll();
+	}
+
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("user").password("password").roles("USER")
+				.username("root").password("root").roles("ADMIN")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+}
