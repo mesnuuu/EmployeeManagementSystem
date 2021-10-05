2ea873ebe80abe990b54de8b5d8984520f156d8e
mesnuuu <mesnuuu@gmail.com>
2021-10-06 01:18:35 +0500

    06.4 - In-memory Authentication - Define and Display User Roles and Id.
    must include
    
    <artifactId>thymeleaf-extras-springsecurity5</artifactId>

--- src/main/java/com/mesnu/ems/WebSecurityConfig.java
+++ src/main/java/com/mesnu/ems/WebSecurityConfig.java
@@ -1,42 +1,53 @@
 package com.mesnu.ems;
 
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.anyRequest().authenticated()
-				.and()
+
+		http.authorizeRequests()
+			.antMatchers("/employees/showForm*").hasAnyRole("MANAGER", "ADMIN")
+			.antMatchers("/employees/save*").hasAnyRole("MANAGER", "ADMIN")
+			.antMatchers("/employees/delete").hasRole("ADMIN")
+			.antMatchers("/employees/**").hasRole("EMPLOYEE")
+			.antMatchers("/resources/**").permitAll()
+			.and()
 			.formLogin()
 				.loginPage("/customLogin")
+				
 				.permitAll()
-				.and()
-			.logout()
-				.permitAll();
+			.and()
+			.logout().permitAll()
+			.and()
+			.exceptionHandling().accessDeniedPage("/access-denied");
+		
 	}
 
-	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user").password("password").roles("USER")
-				.username("root").password("root").roles("ADMIN")
-				.build();
+	
 
-		return new InMemoryUserDetailsManager(user);
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		// add our users for in memory authentication
+		
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+			.withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
+			.withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
+		
 	}
 }
