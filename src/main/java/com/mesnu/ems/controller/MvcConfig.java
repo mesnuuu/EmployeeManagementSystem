7dd8283ff11ecc916f7d10ad779aef50d1569bb0
mesnuuu <mesnuuu@gmail.com>
2021-10-12 19:01:16 +0500

    07 - Consuming Rest with Rest Template in login page

--- src/main/java/com/mesnu/ems/controller/MvcConfig.java
+++ src/main/java/com/mesnu/ems/controller/MvcConfig.java
@@ -1,20 +1,52 @@
 package com.mesnu.ems.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.mesnu.ems.consumeRest.Quote;
+
 @Configuration
+@Controller
 public class MvcConfig implements WebMvcConfigurer {
 
 	
 	  public void addViewControllers(ViewControllerRegistry registry) {
-
-
-	  registry.addViewController("/customLogin").setViewName("my-login");
+		  
+	//  registry.addViewController("/customLogin").setViewName("my-login");
 	  registry.addViewController("/access-denied").setViewName("access-denied");
 	  
 	  }
+	  
+		@Bean
+		public RestTemplate restTemplate() {
+			return new RestTemplate();
+		}
+
+		@Autowired
+		RestTemplate restTemplate;
+		
+		@GetMapping("/customLogin")
+		public String home(Model model) {
+
+			
+			  Quote quote = restTemplate.getForObject(
+			  "https://quoters.apps.pcfone.io/api/random", Quote.class);
+			  
+			  String quoteString = quote.getValue().getQuote();
+			  
+			  model.addAttribute("quote", quoteString) ;
+			 
+
+			return "my-login";
+
+		}
 	 
 
 }
